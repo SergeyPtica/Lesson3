4301e20430c9d1646d10eda38e04ff7159cb0bfe
Serg <s.ostafiichuk@gmail.com>
2017-03-16 16:53:26 +0200

    First version, 3

--- src/TestTax.java
+++ src/TestTax.java
@@ -5,13 +5,15 @@
 public class TestTax {
     public static void main(String[] args) {
 
-        Tax t = new Tax();
+        NJTax t= new NJTax();
 
         t.grossIncome=50000;
         t.dependents=2;
         t.state="NJ";
 
         double yourTax=t.calcTax();
+        double totalTax = t.adjustForStudents(yourTax);
         System.out.println("Your tax is " + yourTax);
+        System.out.println("Your total tax is " + totalTax);
     }
 }
