bdf8fea73cf6423b900ef39a04bbb31cb9952ba8
Serg <s.ostafiichuk@gmail.com>
2017-03-16 22:35:53 +0200

    Latest version with all updates

--- src/TestTax.java
+++ src/TestTax.java
@@ -11,8 +11,8 @@ public class TestTax {
         t.dependents=2;
         t.state="NJ";
 
-        double yourTax=t.calcTax(); // revoke calcTax from Tax class
-        double totalTax = t.adjustForStudents(yourTax); // revoke from NJTax class
+        double yourTax=t.calcTax(); // invoke calcTax from Tax class
+        double totalTax = t.adjustForStudents(yourTax); // invoke from NJTax class
         System.out.println("Your tax is " + yourTax);
         System.out.println("Your total tax is " + totalTax);
     }
